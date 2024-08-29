c0232b8fe2842c16f131541e2c9163645b94e448
Jake <jakea1865@gmail.com>
2024-08-29 11:49:26 -0700

    Add Player class

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,1 +1,22 @@
 #blank
+
+class Player
+  def initialize 
+    @@winner = false
+  end
+end
+
+class Game
+  def initialize
+    @@turn = 1
+    @@board = ['_|_|_',
+    '_|_|_',
+    ' | | ']
+  end
+  def board
+    @@board
+  end
+end
+
+newgame = Game.new
+puts newgame.board
